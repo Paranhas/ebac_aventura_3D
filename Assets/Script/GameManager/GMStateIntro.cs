18e8c984c6724f1002399bfff6ba4f2f2a66ce3b
Paranhas <rodrigoparanhas.br@gmail.com>
2026-05-03 15:58:50 -0300

    Update Modulo StateMachine

---
+++ Assets/Script/GameManager/GMStateIntro.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPStudio.StateMachine;
+
+public class GMStateIntro : StateBase
+{
+
+}
---
+++ Assets/Script/RPStudio/StateMachine/StateBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace RPStudio.StateMachine
+{
+    public class StateBase
+    {
+        public virtual void OnStateEnter(object o = null)
+        {
+            Debug.Log("OnStateEnter");
+        }
+        public virtual void OnStateStay()
+        {
+            Debug.Log("OnStateStay");
+        }
+        public virtual void OnStateExit()
+        {
+            Debug.Log("OnStateExit");
+        }
+    }
+}
