f93c1dd816913059197e71d33da9c03a710c0e8d
Paranhas <rodrigoparanhas.br@gmail.com>
2026-06-09 14:07:14 -0300

    Update EnemyM31

---
+++ Assets/Script/Enimies/EnemyBase.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Animation;
+
+
+namespace Enemy
+{
+    public class EnemyBase : MonoBehaviour, IDamageable
+    {
+        //nas aulas esse colliderDamage é collider
+        public Collider colliderDamage;
+        public FlashColor flashColor;
+        public ParticleSystem particleSystem;
+        public float startLife = 10f;
+        
+        [SerializeField]public float _currentLife;
+
+        [Header("Animation")]
+        [SerializeField] private AnimationBase _animationBase;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = 0.2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool starWithBornAnimation = true;
+        private void Awake()
+        {
+            Init();
+        }
+        protected void ResetLife() 
+        {
+            _currentLife = startLife;
+        }
+
+        protected virtual void Init()
+        {
+            ResetLife();
+            if(starWithBornAnimation)
+            BornAnimation();
+        }
+        protected void Kill()
+        {
+            OnKill();
+        }
+        protected virtual void OnKill() 
+        {
+            if(colliderDamage != null) colliderDamage.enabled = false;
+            Destroy(gameObject, 3f);
+            PlayAnimationByTrigger(AnimationType.DEATH);
+        }
+
+        public void OnDamage(float f)
+        {
+            if(flashColor != null)flashColor.Flash();
+            if (particleSystem != null) particleSystem.Emit(7);
+            transform.position -= transform.forward;
+            _currentLife -= f;
+            if(_currentLife <= 0) 
+            {
+                Kill();
+            }
+        }
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+        public void PlayAnimationByTrigger(AnimationType animationType)
+        {
+                _animationBase.PlayAnimationByTrigger(animationType);
+        }
+
+        #endregion
+        public void Damage(float damage)
+        {
+            OnDamage(damage);
+        }
+        public void Damage(float damage, Vector3 dir)
+        {
+            OnDamage(damage);
+            transform.DOMove(transform.position - dir, .1f);
+        }
+
+
+    }
+}
+
--- Assets/Script/Gun/ProjectileBase.cs
+++ Assets/Script/Gun/ProjectileBase.cs
@@ -19,5 +19,15 @@ public class ProjectileBase : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        var damageable = collision.transform.GetComponent<IDamageable>();
+        if(damageable != null) 
+        {
+            Vector3 dir = collision.transform.position - transform.position;
+            dir = -dir.normalized;
+            dir.y = 0;
+            damageable.Damage(damageAmount, dir);
+            Destroy(gameObject);
+        }
+        
     }
 }
