394f5adae706df5226746dfb68a6e5de2a680054
Paranhas <rodrigoparanhas.br@gmail.com>
2026-08-01 12:18:24 -0300

    update aula 39.1 e 39.2

---
+++ Assets/Script/SaveManager/SaveManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using RPStudio.Core.Singleton;
+
+public class SaveManager : Singleton<SaveManager>
+{
+    private SaveSetup _saveSetup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _saveSetup = new SaveSetup();
+        _saveSetup.lastLevel = 0;
+        _saveSetup.playerName = "Player1";
+    }
+    #region SAVE
+    [NaughtyAttributes.Button]
+    private void Save() 
+    {
+        string setupToJson = JsonUtility.ToJson(_saveSetup, true);
+        Debug.Log(setupToJson);
+        SaveFile(setupToJson);
+    }
+
+    private void SaveLastLevel(int level )
+    {
+        _saveSetup.lastLevel = level;
+        Save();
+    }
+    #endregion
+
+    private void SaveFile(string json) 
+    {
+        string path = Application.persistentDataPath +"/save.txt";
+
+        Debug.Log(path);
+        File.WriteAllText(path, json);
+    }
+
+    [NaughtyAttributes.Button]
+    private void SaveLevelOne()
+    {
+        SaveLastLevel(1);
+    }
+
+
+}
+
+[System.Serializable]
+public class SaveSetup
+{
+    public int lastLevel;
+    public string playerName;
+}
